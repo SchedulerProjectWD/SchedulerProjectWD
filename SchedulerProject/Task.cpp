ff5c961dd4c37bdd1fb68512846be78682468d81
miri <mendelson.miri@gmail.com>
2022-08-02 11:02:28 +0300

    create Task class

---
+++ SchedulerProject/Task.cpp
@@ -0,0 +1,32 @@
+#include "Task.h"
+
+bool operator<(const Task& task1, const Task& task2)
+{
+    if (task1.getPriority() == task2.getPriority())
+        return task1.getArriavlTime() < task2.getArriavlTime();
+    return task1.getPriority() < task2.getPriority();
+}
+
+bool operator>(const Task& task1, const Task& task2)
+{
+    if (task1.getPriority() == task2.getPriority())
+        return task1.getArriavlTime() > task2.getArriavlTime();
+    return task1.getPriority() > task2.getPriority();
+}
+
+Task::Task(int taskId, int type, int (*func)(void*), 
+            int arriavlTime, int priority, void* args)
+{
+    this->taskId = taskId;
+    this->type = (Task::eType)type;
+    this->func = *func;
+    this->arriavlTime = arriavlTime;
+    this->priority = priority;
+    this->args = args;
+}
+
+bool Task::Start()
+{
+    func(args);
+    return true;
+}
---
+++ SchedulerProject/Task.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef _TASK_H_
+#define _TASK_H_
+
+class Task
+{
+public:
+	Task( int taskId, int type, int (*func)(void*), 
+		  int arriavlTime, int priority, void* args = nullptr);
+	enum eType { real_time, low, high };
+	int getTaskId() const { return this->taskId; }
+	eType getType() const { return this->type; }
+	auto getFunc() const { return this->func; }
+	int getArriavlTime() const { return this->arriavlTime; }
+	int getPriority() const { return this->priority; }
+	friend bool operator<(const Task& task1, const Task& task2);
+	friend bool operator>(const Task& task1, const Task& task2);
+	bool Start();
+private:
+	void* args;
+	int taskId;
+	eType type;
+	int (*func)(void*);
+	int arriavlTime;
+	int priority;
+};
+#endif // !_TASK_H_
+
