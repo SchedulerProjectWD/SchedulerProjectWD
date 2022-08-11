de15e08413e1fd023afa1f11a3e05239b5c6d514
miri <mendelson.miri@gmail.com>
2022-08-11 09:46:13 +0300

    update taskTest

--- SchedulerUnitTests/TaskTest.cpp
+++ SchedulerUnitTests/TaskTest.cpp
@@ -7,19 +7,22 @@ int demoFunc(void*) {
 
 class TaskTest : public ::testing::Test {
 protected:
-	void SetUp() override {
-
-	}
-	Task* RTtask1 = new Task(1, 1, demoFunc, 1, 3, 50);
-	Task* RTtask2 = new Task(2, 1, demoFunc, 2, 4, 50);
+	//Task(int taskId, int type, int (*func)(void*), int arriavlTime,
+	//	int timeOut, int priority, void* args);
+	Task* RTtask1 = new Task(1, 1, demoFunc, 1, 3, 50, nullptr);
+	Task* RTtask2 = new Task(2, 1, demoFunc, 2, 4, 50, nullptr);
 	~TaskTest() {
 		delete RTtask1;
 		delete RTtask2;
 	}
 };
 TEST_F(TaskTest, TestOerator){
-	bool isGrater = RTtask1 < RTtask2;
-	EXPECT_EQ(isGrater, false);
-	bool isGrater2 = RTtask1 > RTtask2;
+	bool isSmaller = *RTtask1 < *RTtask2;
+	EXPECT_EQ(isSmaller, false);
+	bool isGrater2 = *RTtask1 > *RTtask2;
 	EXPECT_EQ(isGrater2, true);
+	/*bool isGrater2 = (RTtask1 > 40);
+	EXPECT_EQ(isGrater2, true);*/
 };
+
+
