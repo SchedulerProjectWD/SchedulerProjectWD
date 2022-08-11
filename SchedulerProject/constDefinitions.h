e734458b3b3a9e0d79e0a7b899e1933af8ea99f2
SchneiderRachel <r0534108825@gmail.com>
2022-08-11 11:34:56 +0300

    const definitions

--- SchedulerProject/constDefinitions.h
+++ SchedulerProject/constDefinitions.h
@@ -1,8 +1,8 @@
 #pragma once
 const int QUEUES_COUNT = 3;
-const int CLOSE_TO_STARVATION_HIGH = 3;
-const int CLOSE_TO_STARVATION_LOW = 3;
-const int CLOSE_TO_STARVATION_RT = 3;
+const int CLOSE_TO_STARVATION_HIGH = 5;
+const int CLOSE_TO_STARVATION_LOW = 20;
+const int CLOSE_TO_STARVATION_RT = 10;
 const int PRIORITY_INVERSION_AVOID = 5;
 const int MAX_CAPACITY = 200;
 const int TIME_TO_DETECT_SYSTEM = 20;
